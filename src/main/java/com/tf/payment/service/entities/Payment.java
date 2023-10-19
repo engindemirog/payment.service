24acfe9a8f1a69774077c170993e0155408eb534
Engin Demiroğ <engindemirog@gmail.com>
2023-10-19 11:27:15 +0300

    Add Payment Entity

---
+++ src/main/java/com/tf/payment/service/entities/Payment.java
@@ -0,0 +1,37 @@
+package com.tf.payment.service.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @Column(name = "id")
+    private String id;
+    @Column(name = "customer_id")
+    private String customerId;
+    @Column(name = "card_number")
+    private String cardNumber;
+    @Column(name = "cardholder")
+    private String cardholder;
+    @Column(name = "card_expiration_year")
+    private int cardExpirationYear;
+    @Column(name = "card_expiration_month")
+    private int cardExpirationMonth;
+    @Column(name = "card_cvv")
+    private String cardCvv;
+    @Column(name = "balance")
+    private double balance;
+}
